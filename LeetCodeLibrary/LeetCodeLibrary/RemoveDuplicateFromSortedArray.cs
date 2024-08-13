0b6e14dfbaa080b8a5df619d368bab9b0c6e8b51
Joshua Vazquez <106997964+joshua-vazquez@users.noreply.github.com>
2024-08-13 15:52:39 -0500

    Created a solution to challenge 26 on leetcode, along with an NUnit test.

---
+++ LeetCodeLibrary/LeetCodeLibrary/RemoveDuplicateFromSortedArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLibrary
+{
+    public class RemoveDuplicateFromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            //If the array is empty return 0
+            if ( nums.Length == 0)
+            {
+                return 0;
+            }
+
+            // Initialize a pointer k to 0 to track the position of the last unique element
+            int k = 0;
+
+            // Iterate through the array starting from the second element
+            for (int  j = 1;  j < nums.Length;  j++)
+            {
+                // If the current element is different from the last unique element 
+                if (nums[j] != nums[k])
+                {
+                    k++; // move the pointer k to the next position
+                    nums[k] = nums[j]; //Update the element at position k to the current element
+                }
+            }
+            // Return the number of unique elements, which is k + 1
+            return k + 1;
+        }
+    }
+}
---
+++ LeetCodeLibrary/LeetCodeLibraryTests/RemoveDuplicateFromSortedArrayTests.cs
@@ -0,0 +1,72 @@
+﻿using LeetCodeLibrary;
+using NUnit.Framework;
+
+namespace LeetCodeLibraryTests
+{
+    [TestFixture]
+    public class RemoveDuplicateFromSortedArrayTests
+    {
+        [Test]
+        public void RemoveDuplicates_RemovesDuplicates_Correctly()
+        {
+            // Arrange
+            var solution = new RemoveDuplicateFromSortedArray();
+            int[] nums1 = { 1, 1, 2 };
+            int[] expected1 = { 1, 2 };
+            int expectedLength1 = 2;
+
+            int[] nums2 = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            int[] expected2 = { 0, 1, 2, 3, 4 };
+            int expectedLength2 = 5;
+
+            // Act
+            int resultLength1 = solution.RemoveDuplicates(nums1);
+            int resultLength2 = solution.RemoveDuplicates(nums2);
+
+            // Assert
+            Assert.That(expectedLength1, Is.EqualTo(resultLength1));
+            for (int i = 0; i < expectedLength1; i++)
+            {
+                Assert.That(expected1[i], Is.EqualTo(nums1[i]));
+            }
+
+            Assert.That(expectedLength2, Is.EqualTo(resultLength2));
+            for (int i = 0; i < expectedLength2; i++)
+            {
+                Assert.That(expected2[i], Is.EqualTo(nums2[i]));
+            }
+        }
+
+        [Test]
+        public void RemoveDuplicates_EmptyArray_ReturnsZero()
+        {
+            // Arrange
+            var solution = new RemoveDuplicateFromSortedArray();
+            int[] nums = { };
+            int expectedLength = 0;
+
+            // Act
+            int resultLength = solution.RemoveDuplicates(nums);
+
+            // Assert
+            Assert.That(expectedLength, Is.EqualTo(resultLength));
+        }
+
+        [Test]
+        public void RemoveDuplicates_SingleElementArray_ReturnsOne()
+        {
+            // Arrange
+            var solution = new RemoveDuplicateFromSortedArray();
+            int[] nums = { 1 };
+            int[] expected = { 1 };
+            int expectedLength = 1;
+
+            // Act
+            int resultLength = solution.RemoveDuplicates(nums);
+
+            // Assert
+            Assert.That(expectedLength, Is.EqualTo(resultLength));
+            Assert.That(expected[0], Is.EqualTo(nums[0]));
+        }
+    }
+}
