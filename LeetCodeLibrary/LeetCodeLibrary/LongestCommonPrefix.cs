4f108e126232b41eed69e8a2b4a4d0f4ca5322ca
Joshua Vazquez <106997964+joshua-vazquez@users.noreply.github.com>
2024-08-14 15:31:47 -0500

    Created a solution to the LeetCode challenge 14, along with some test cases.

---
+++ LeetCodeLibrary/LeetCodeLibrary/LongestCommonPrefix.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLibrary
+{
+    public class LongestCommonPrefix
+    {
+        public string FindLongestCommonPrefix(string[] strs)
+        {
+            // If the input is null or empty, return an empty string
+            if (strs == null || strs.Length == 0)
+            {
+                return "";
+            }
+
+            // Iterate through the characters of the first string
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                // Get the character at the current index
+                char c = strs[0][i];
+
+                // Compare the character with the corresponding character in the other strings
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    // If the index is out of bounds or the characters don't match, return the prefix
+                    if (i == strs[j].Length || strs[j][i] != c)
+                    {
+                        return strs[0].Substring(0, i);
+                    }
+                }
+            }
+            // Return the first string if all characters match
+            return strs[0];
+        }
+    }
+}
---
+++ LeetCodeLibrary/LeetCodeLibraryTests/LongestCommonPrefixTests.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using LeetCodeLibrary;
+using System.Globalization;
+
+namespace LeetCodeLibraryTests
+{
+    [TestFixture]
+    public class LongestCommonPrefixTests
+    {
+        private LongestCommonPrefix longestCommonPrefix;
+
+        [SetUp]
+        public void SetUp()
+        {
+            longestCommonPrefix = new LongestCommonPrefix();
+        }
+
+        [Test]
+        public void TestCommonPrefix()
+        {
+            string[] strs = { "flower", "flow", "flight" };
+            
+            string expected = "fl";
+            string result = longestCommonPrefix.FindLongestCommonPrefix(strs);
+            
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void TestNoCommonPrefix()
+        {
+            string[] strs = { "dog", "racecar", "car" };
+            string expected = "";
+
+            string result = longestCommonPrefix.FindLongestCommonPrefix(strs);
+            
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void TestEmptyArray()
+        {
+            string[] strs = { };
+            string expected = "";
+
+            string result = longestCommonPrefix.FindLongestCommonPrefix(strs);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void TestSingleString()
+        {
+            string[] strs = { "single" };
+            string expected = "single";
+
+            string result = longestCommonPrefix.FindLongestCommonPrefix(strs);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void TestAllStringsIdentical()
+        {
+            string[] strs = { "test", "test", "test" };
+            string expected = "test";
+
+            string result = longestCommonPrefix.FindLongestCommonPrefix(strs);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void TestMixedLengthStrings()
+        {
+            string[] strs = { "ab", "a" };
+            string expected = "a";
+
+            string result = longestCommonPrefix.FindLongestCommonPrefix(strs);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+}
