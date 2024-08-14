0c1b6acd8e716e4cd6d7fa4f052a0234105badae
Joshua Vazquez <106997964+joshua-vazquez@users.noreply.github.com>
2024-08-14 12:38:15 -0500

    Created a class and NUnit test for LeetCode challenge 13.

---
+++ LeetCodeLibrary/LeetCodeLibrary/RomanToInteger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLibrary
+{
+    public class RomanToInteger
+    {
+        public static int RomanToInt(string s)
+        {
+            // Create a dictionary to map roman numerals to integers
+            Dictionary<char, int> romanMap = new Dictionary<char, int>
+            { 
+                {'I', 1},
+                { 'V', 5},
+                { 'X', 10},
+                { 'L', 50},
+                { 'C', 100},
+                { 'D', 500},
+                { 'M', 1000}
+            };
+
+            // Initialize a variable to store the total
+            int total = 0;
+
+            // Iterate through the string
+            for (int i = 0; i < s.Length; i++)
+            {
+                // If the current roman numeral is less than the next one, subtract it from the total
+                if (i + 1 < s.Length && romanMap[s[i]] < romanMap[s[i + 1]])
+                {
+                    total -= romanMap[s[i]];
+                }
+                // Otherwise, add it to the total
+                else
+                {
+                    total += romanMap[s[i]];
+                }
+            }
+            // Return the total
+            return total;
+        }
+    }
+}
---
+++ LeetCodeLibrary/LeetCodeLibraryTests/RomanToIntegerTests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using LeetCodeLibrary;
+
+namespace LeetCodeLibraryTests
+{
+    [TestFixture]
+    public class RomanToIntegerTests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            string roman = "III";
+            int expected = 3;
+
+            int result = RomanToInteger.RomanToInt(roman);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void MediumTest()
+        {
+            string roman = "LVIII";
+            int expected = 58;
+
+            int result = RomanToInteger.RomanToInt(roman);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void ComplexTest()
+        {
+            string roman = "MCMXCIV";
+            int expected = 1994;
+
+            int result = RomanToInteger.RomanToInt(roman);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void SingleRomanNumeral()
+        {
+            string roman = "V";
+            int expected = 5;
+
+            int result = RomanToInteger.RomanToInt(roman);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void TestSubtractiveNotation()
+        {
+            string roman = "IX";
+            int expected = 9;
+
+            int result = RomanToInteger.RomanToInt(roman);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        public void MostComplexTest()
+        {
+            string roman = "MMMDCCCLXXXVIII";
+            int expected = 3888;
+
+            int result = RomanToInteger.RomanToInt(roman);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+}
