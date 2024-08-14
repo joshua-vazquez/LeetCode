48aa4b512cb4b38957fef0db4c4a8b7e706b7252
Joshua Vazquez <106997964+joshua-vazquez@users.noreply.github.com>
2024-08-14 16:20:57 -0500

    Completed the solution for LeetCode challenge 28.

--- LeetCodeLibrary/LeetCodeLibrary/NeedleInAHaystack.cs
+++ LeetCodeLibrary/LeetCodeLibrary/NeedleInAHaystack.cs
@@ -11,7 +11,29 @@ namespace LeetCodeLibrary
         public int StrStr(string haystack, string needle)
         {
             // If the needle is in an empty string, return 0
+            if (string.IsNullOrEmpty(needle))
+            {
+                return 0;
+            }
 
+            // If the haystack is shorter than the needle, return -1
+            if (haystack.Length < needle.Length)
+            {
+                return -1;
+            }
+
+            // Iterate through the haystack with a window of the same length as the needle
+            for (int i = 0; i <= haystack.Length - needle.Length; i++)
+            {
+                // Check if the substring of the haystack matches the needle
+                if (haystack.Substring(i, needle.Length) == needle)
+                {
+                    return i;
+                }
+            }
+
+            // If the needle is not found, return -1
+            return -1;
         }
     }
 }
