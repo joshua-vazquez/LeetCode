1cfc7dd0eaa1168a2179eb81aa7ba52d7f32dd79
Joshua Vazquez <106997964+joshua-vazquez@users.noreply.github.com>
2024-08-14 16:18:23 -0500

    Created ArrayString directory to hold all the challenges related to the topic, and ArrayStrings directory to hold all the test cases.

---
+++ LeetCodeLibrary/LeetCodeLibrary/NeedleInAHaystack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLibrary
+{
+    public class NeedleInAHaystack
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            // If the needle is in an empty string, return 0
+
+        }
+    }
+}
