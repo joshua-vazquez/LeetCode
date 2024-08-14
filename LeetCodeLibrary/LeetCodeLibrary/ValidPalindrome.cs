f669efd1602c8d35859e6069e7cf7bda986812ac
Joshua Vazquez <106997964+joshua-vazquez@users.noreply.github.com>
2024-08-14 16:57:21 -0500

    Completed the solution for LeetCode challenge 125, along with some test cases.

---
+++ LeetCodeLibrary/LeetCodeLibrary/ValidPalindrome.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLibrary
+{
+    public class ValidPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            // Preprocess the string
+            s = Preprocess(s);
+
+            // Initialize our Two Pointers
+            int start = 0;
+            int end = s.Length - 1;
+
+            // Compare the characters at the start and end of the string
+            while (start < end)
+            {
+                // If the characters don't match, return false
+                if (s[start] != s[end])
+                {
+                    return false;
+                }
+
+                // Move the pointers towards the center
+                start++;
+                end--;
+            }
+
+            // If all characters match, return true
+            return true;
+        }
+
+        // Helper method to remove non alphanumeric characters from the string
+        private string Preprocess(string s)
+        {
+            var sb = new StringBuilder();
+            foreach (char c in s)
+            {
+                if (char.IsLetterOrDigit(c))
+                {
+                    sb.Append(char.ToLower(c));
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ LeetCodeLibrary/LeetCodeLibraryTests/ValidPalindromeTests.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+using LeetCodeLibrary;
+
+
+namespace LeetCodeLibraryTests
+{
+    [TestFixture]
+    public class ValidPalindromeTests
+    {
+        private ValidPalindrome validPalindrome;
+
+        [SetUp]
+        public void SetUp()
+        {
+            validPalindrome = new ValidPalindrome();    
+        }
+
+        [Test]
+        public void BasicTest()
+        {
+            string s = "racecar";
+
+            bool result = validPalindrome.IsPalindrome(s);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void NonPalindrome()
+        {
+            string s = "hello";
+
+            bool result = validPalindrome.IsPalindrome(s);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void EmptyString()
+        {
+            string s = "";
+
+            bool result = validPalindrome.IsPalindrome(s);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void SingleCharacter()
+        {
+            string s = "a";
+
+            bool result = validPalindrome.IsPalindrome(s);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void NonAlphanumericCharacters()
+        {
+            string s = "A man, a plan, a canal: Panama";
+
+            bool result = validPalindrome.IsPalindrome(s);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void MixedCasePalindrome()
+        {
+            string s = " Able was I ere I saw Elba";
+
+            bool result = validPalindrome.IsPalindrome(s);
+
+            Assert.That(result, Is.True);
+        }
+
+    }
+}
