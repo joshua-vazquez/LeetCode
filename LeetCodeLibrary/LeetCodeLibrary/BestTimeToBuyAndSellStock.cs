beac2f8f040415145a67fd9c32fef076710337db
Joshua Vazquez <106997964+joshua-vazquez@users.noreply.github.com>
2024-08-14 11:53:06 -0500

    Created a solution for challenge 121.

---
+++ LeetCodeLibrary/LeetCodeLibrary/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLibrary
+{
+    public class BestTimeToBuyAndSellStock
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+
+            for (int  i = 0;  i < prices.Length;  i++)
+            {
+                if (prices[i] < minPrice)
+                {
+                    minPrice = prices[i];
+                }
+                else if (prices[i] - minPrice > maxProfit)
+                {
+                  maxProfit = prices[i] - minPrice;
+                }
+            }
+            return maxProfit;
+        }
+    }
+}
