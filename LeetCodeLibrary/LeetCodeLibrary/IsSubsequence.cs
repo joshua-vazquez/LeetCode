aa1142d48ac884bf0e08945aaf79131224b7338d
Joshua Vazquez <106997964+joshua-vazquez@users.noreply.github.com>
2024-08-15 11:34:28 -0500

    Created a solution for the LeetCode challenge 392, along with some test cases.

---
+++ LeetCodeLibrary/LeetCodeLibrary/IsSubsequence.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLibrary
+{
+    public class IsSubsequence
+    {
+        public bool FindSubsequence(string s, string t)
+        {
+            /*Initialize both of our pointers
+           int i = 0;
+           int j = 0;
+
+            Loop through the strings while i is less than the length of s and j is less than the length of t
+           while ( i < s.Length && j < t.Length)
+           {
+                If the characters at the current index of s and t match, increment i
+               if (s[i] == t[j])
+               {
+                   i++;
+               }
+                We always increment j to move through t
+               j++;
+           }
+            If i is equal to the length of s, we have found a subsequence
+           return i == s.Length;*/
+
+
+            // Preprocess the string t
+            var charPositions = new Dictionary<char, List<int>>();
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (!charPositions.ContainsKey(t[i]))
+                {
+                    charPositions[t[i]] = new List<int>();
+                }
+                charPositions[t[i]].Add(i);
+            }
+
+            // Check each character of s
+            int prevPosition = -1;
+            foreach (char c in s)
+            {
+                if (!charPositions.ContainsKey(c))
+                {
+                    return false;
+                }
+
+                var positions = charPositions[c];
+                int nextPosition = BinarySearch(positions, prevPosition);
+
+                if (nextPosition == -1)
+                {
+                    return false;
+                }
+                prevPosition = nextPosition;
+            }
+            return true;
+        }
+
+        // Create a helper method to perform a binary search on the list of positions
+        private int BinarySearch(List<int> positions, int prevPosition)
+        {
+            int left = 0;
+            int right = positions.Count - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (positions[mid] <= prevPosition)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return left < positions.Count ? positions[left] : -1;
+        }
+    }
+}
---
+++ LeetCodeLibrary/LeetCodeLibraryTests/IsSubsequenceTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using LeetCodeLibrary;
+
+namespace LeetCodeLibraryTests
+{
+    [TestFixture]
+    public class IsSubsequenceTests
+    {
+        private IsSubsequence isSubsequence;
+        [SetUp]
+        public void SetUp()
+        {
+            isSubsequence = new IsSubsequence();
+        }
+
+        [Test]
+        public void BasicTest()
+        {
+            string s = "abc";
+            string t = "ahbgdc";
+            bool expected = true;
+
+            bool result = isSubsequence.FindSubsequence(s, t);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void BasicTest2() 
+        {
+            string s = "axc";
+            string t = "ahbgdc";
+            bool expected = false;
+
+            bool result = isSubsequence.FindSubsequence(s, t);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void EmptyString()
+        {
+            string s = "";
+            string t = "ahbgdc";
+            bool expected = true;
+
+            bool result = isSubsequence.FindSubsequence(s, t);
+
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+}
