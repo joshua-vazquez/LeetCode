5da67ff3646e82a89c76a69931b302d48ba335f8
Joshua Vazquez <106997964+joshua-vazquez@users.noreply.github.com>
2024-08-15 17:41:44 -0500

    Created a solution for LeetCode challenge 209, along with some test cases.

---
+++ LeetCodeLibrary/LeetCodeLibrary/WordPattern.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLibrary
+{
+    public class WordPattern
+    {
+        public bool IsThereAPAttern(string pattern, string s)
+        {
+            // Split the string into words
+            var words = s.Split(' ');
+
+            // Check if the pattern and the words have the same length
+            if (pattern.Length != words.Length)
+            {
+                return false;
+            }
+
+            // Initialize the maps(aka dictionaries)
+            var mapCharToWord = new Dictionary<char, string>();
+            var mapWordToChar = new Dictionary<string, char>();
+
+            // Check if the pattern and the words match
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                char c = pattern[i];
+                string word = words[i];
+
+                if (mapCharToWord.ContainsKey(c))
+                {
+                    if (mapCharToWord[c] != word)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    mapCharToWord[c] = word;
+                }
+
+                if (mapWordToChar.ContainsKey(word))
+                {
+                    if (mapWordToChar[word] != c)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    mapWordToChar[word] = c;
+                }
+            }
+
+            // If the pattern and the words match, return true
+            return true;
+        }
+    }
+}
---
+++ LeetCodeLibrary/LeetCodeLibraryTests/WordPatternTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using LeetCodeLibrary;
+
+namespace LeetCodeLibraryTests
+{
+    [TestFixture]
+    public class WordPatternTests
+    {
+        private WordPattern wordPattern;
+
+        [SetUp]
+        public void SetUp()
+        {
+            wordPattern = new WordPattern();
+        }
+
+        [Test]
+        [TestCase("abba", "dog cat cat dog", true)]
+        [TestCase("lololol", "cat dog cat dog cat dog cat", true)]
+        public void TestIsThereAPaternTrue (string pattern, string s, bool expected)
+        {
+            bool result = wordPattern.IsThereAPAttern(pattern, s);
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        [Test]
+        [TestCase("abba", "dog cat cat fish", false)]
+        [TestCase("lololol", "cat dog cat dog cat fish", false)]
+        public void TestIsThereAPaternFalse(string pattern, string s, bool expected)
+        {
+            bool result = wordPattern.IsThereAPAttern(pattern, s);
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+}
