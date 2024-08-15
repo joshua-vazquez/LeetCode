033fd8c825036c5c3eda6db22a5514c717289d59
Joshua Vazquez <106997964+joshua-vazquez@users.noreply.github.com>
2024-08-15 15:47:49 -0500

    Created a solution for LeetCode challenge 205, along with test cases.

---
+++ LeetCodeLibrary/LeetCodeLibraryTests/IsomorphicStringTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using LeetCodeLibrary;
+
+namespace LeetCodeLibraryTests
+{
+    [TestFixture]
+    public class IsomorphicStringTests
+    {
+        private IsomorphicString isomorphicString;
+
+        [SetUp]
+        public void SetUp()
+        {
+            isomorphicString = new IsomorphicString();
+        }
+
+        [Test]
+        [TestCase("egg", "add", true)]
+        [TestCase("paper", "title", true)]
+        public void TestIsIsomorphic(string s, string t, bool expected)
+        {
+            bool result = isomorphicString.IsIsomorphic(s, t);
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        [Test]
+        [TestCase("foo", "bar", false)]
+        [TestCase("ab", "aa", false)]
+        public void TestIsIsomorphicFalse(string s, string t, bool expected)
+        {
+            bool result = isomorphicString.IsIsomorphic(s, t);
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        [Test]
+        [TestCase("b", "a", true)]
+        [TestCase("a", "a", true)]
+        public void TestIsIsomorphicSingleChar(string s, string t, bool expected)
+        {
+            bool result = isomorphicString.IsIsomorphic(s, t);
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+}
