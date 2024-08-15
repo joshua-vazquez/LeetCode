947e44c2b7b160ab2e2bd50ee3d919d92bbcbe96
Joshua Vazquez <106997964+joshua-vazquez@users.noreply.github.com>
2024-08-15 12:37:21 -0500

    Created a solution to LeetCode challenge 383, alon with some test cases.

---
+++ LeetCodeLibrary/LeetCodeLibrary/RansomeNote.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLibrary
+{
+    public class RansomeNote
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            // Initializa a dictionary to count the frequency of each character in the magazine
+            var charCount = new Dictionary<char, int>();
+
+            // Count the frequency of each character in the magazine
+            foreach (char c in magazine)
+            {
+                if (!charCount.ContainsKey(c))
+                {
+                    charCount[c] = 0;
+                }
+                charCount[c]++;
+            }
+
+            // Check if the ransomNote can be constructed
+            foreach (char c in ransomNote)
+            {
+                if (!charCount.ContainsKey(c) || charCount[c] == 0)
+                {
+                    return false;
+                }
+                charCount[c]--;
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCodeLibrary/LeetCodeLibraryTests/RansomeNoteTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using LeetCodeLibrary;
+
+namespace LeetCodeLibraryTests
+{
+    [TestFixture]
+    public class RansomeNoteTests
+    {
+        private RansomeNote ransomeNote;
+
+        [SetUp]
+        public void SetUp()
+        {
+            ransomeNote = new RansomeNote();
+        }
+
+        [Test]
+        [TestCase("a", "b", false)]
+        [TestCase("aa", "ab", false)]
+        [TestCase("aa", "aab", true)]
+        [TestCase("a", "a", true)]
+        [TestCase("", "aab", true)]
+        [TestCase("a", "", false)]
+        [TestCase("", "", true)]
+        [TestCase("aab", "aab", true)]
+        [TestCase("aabb", "aab", false)]
+        public void TestRansomeNote(string ransomNote, string magazine, bool expected)
+        {
+            bool result = ransomeNote.CanConstruct(ransomNote, magazine);
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+}
