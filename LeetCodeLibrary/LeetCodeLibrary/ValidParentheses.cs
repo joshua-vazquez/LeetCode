a2654c8dfb7d93f6734cfd4a16b2557fa802ff29
Joshua Vazquez <106997964+joshua-vazquez@users.noreply.github.com>
2024-08-19 17:39:33 -0500

    Created solution for LeetCode challenge 20, along with test cases.

---
+++ LeetCodeLibrary/LeetCodeLibrary/ValidParentheses.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLibrary
+{
+    public class ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> parenths = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    parenths.Push(c);
+                }
+                else
+                {
+                    if (parenths.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    char top = parenths.Pop();
+                    if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '['))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return parenths.Count == 0;
+        }
+    }
+}
---
+++ LeetCodeLibrary/LeetCodeLibraryTests/ValidParenthesesTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using LeetCodeLibrary;
+
+namespace LeetCodeLibraryTests
+{
+    [TestFixture]
+    public class ValidParenthesesTests
+    {
+        [Test]
+        [TestCase("()", true)]
+        [TestCase("()[]{}", true)]
+        [TestCase("(]", false)]
+        [TestCase("([)]", false)]
+        [TestCase("{[]}", true)]
+        [TestCase("]", false)]
+        [TestCase("(", false)]
+        public void TestIsValid(string s, bool expected)
+        {
+            bool result = new ValidParentheses().IsValid(s);
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+}
