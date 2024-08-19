dd248b58050c3e23c9f8b11d5e5b235cd5c0d011
Joshua Vazquez <106997964+joshua-vazquez@users.noreply.github.com>
2024-08-19 10:10:53 -0500

    Created a solution for LeetCode challenge 242, along with test cases.

---
+++ LeetCodeLibrary/LeetCodeLibrary/ValidAnagram.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLibrary
+{
+    public class ValidAnagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            // Check if the two strings are the same length
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            // Initialize the count dictionary
+            Dictionary<char, int> count = new Dictionary<char, int>();
+
+            // Iterates through the string s and maps it to the dictionary
+            foreach (char c in s)
+            {
+                if (count.ContainsKey(c))
+                {
+                    count[c]++;
+                }
+                else
+                {
+                    count[c] = 1;
+                }
+            }
+
+            // Iterates through the string t and decrements the count
+            foreach (char c in t)
+            {
+                if (count.ContainsKey(c))
+                {
+                    count[c]--;
+
+                    if (count[c] == 0)
+                    {
+                        count.Remove(c);
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return count.Count == 0;
+        }
+    }
+}
---
+++ LeetCodeLibrary/LeetCodeLibraryTests/ValidAnagramTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using LeetCodeLibrary;
+
+namespace LeetCodeLibraryTests
+{
+    [TestFixture]
+    public class ValidAnagramTests
+    {
+        private ValidAnagram validAnagram;
+
+        [SetUp]
+        public void SetUp()
+        {
+            validAnagram = new ValidAnagram();
+        }
+
+        [Test]
+        [TestCase("anagram", "nagaram", true)]
+        public void TestIsAnagram(string s, string t, bool expected)
+        {
+            bool result = validAnagram.IsAnagram(s, t);
+            Assert.That(expected, Is.EqualTo(result));
+        }
+
+        [Test]
+        [TestCase("rat", "car", false)]
+        public void TestIsNotAnagram(string s, string t, bool expected)
+        {
+            bool result = validAnagram.IsAnagram(s, t);
+            Assert.That(expected, Is.EqualTo(result));
+        }
+    }
+}
