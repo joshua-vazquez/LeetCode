9cf8ecc384c26946b6e1c6c9777310c2d05e7b55
Joshua Vazquez <106997964+joshua-vazquez@users.noreply.github.com>
2024-08-20 12:02:42 -0500

    Created solutions for LeetCode challenge 141, along with test cases.

---
+++ LeetCodeLibrary/LeetCodeLibrary/LinkedListCycle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LeetCodeLibrary
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+    public class LinkedListCycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return false;
+            }
+
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+
+                if (slow == fast)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCodeLibrary/LeetCodeLibraryTests/LinkedListCycleTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using LeetCodeLibrary;
+
+namespace LeetCodeLibraryTests
+{
+    [TestFixture]
+    public class LinkedListCycleTests
+    {
+        [Test]
+        public void TestNoCycle()
+        {
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+
+            bool result = new LinkedListCycle().HasCycle(head);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void TestWithCycle()
+        {
+            ListNode head = new ListNode(1);
+            ListNode second = new ListNode(2);
+            ListNode third = new ListNode(3);
+
+            head.next = second;
+            second.next = third;
+            third.next = second;
+
+            bool result = new LinkedListCycle().HasCycle(head);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void TestEmptyList()
+        {
+            ListNode head = null;
+
+            bool result = new LinkedListCycle().HasCycle(head);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void TestSingleNodeNoCycle()
+        {
+            ListNode head = new ListNode(1);
+
+            bool result = new LinkedListCycle().HasCycle(head);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void TestSingleNodeWithCycle()
+        {
+            ListNode head = new ListNode(1);
+            head.next = head;
+
+            bool result = new LinkedListCycle().HasCycle(head);
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
